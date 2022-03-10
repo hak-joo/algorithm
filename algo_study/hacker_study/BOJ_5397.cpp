b42c9e6b1080e034d1fa009d0e109eb44b49c03a
leehaakjoo <l--h--j--@naver.com>
2022-03-11 01:41:12 +0900

    code modify

--- algo_study/hacker_study/BOJ_5397.cpp
+++ algo_study/hacker_study/BOJ_5397.cpp
@@ -1,36 +1,3 @@
-// #include <iostream>
-// #include <list>
-
-// using namespace std;
-
-// int N;
-
-// int main(void){
-//     cin.tie(0); cout.tie(0);
-//     ios::sync_with_stdio(0);
-
-//     cin >> N;
-//     list<char> l;
-//     string input;
-//     list<char>::iterator iter;
-//     while(N--){
-//         cin >> input;
-//         l.clear();
-//         iter = l.begin();
-
-//         for(int i=0; i<input.length(); i++){
-//             if(input[i] == '<' && iter!=l.begin()) iter--;
-//             else if(input[i] == '>' && iter != l.end()) iter++;
-//             else if(input[i] == '-' && iter != l.begin()) l.erase(--iter);
-//             else if(input[i] != '-' && input[i] != '<' && input[i] != '>'){
-//                 l.insert(iter, input[i]);
-//             }
-//         }
-//         for(iter =l.begin(); iter!=l.end(); iter++) cout<< *iter;
-//         cout<<'\n';
-//     }
-// }
-
 #include <iostream>
 #include <list>
 using namespace std;
